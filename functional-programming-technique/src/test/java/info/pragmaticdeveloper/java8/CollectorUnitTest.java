dea8f5994668783237405e708ccd5e06b6e3d2d9
astha-iOS <asthayadavit@gmail.com>
2023-02-19 09:39:13 +0100

    add calories count for each dish type

--- functional-programming-technique/src/test/java/info/pragmaticdeveloper/java8/CollectorUnitTest.java
+++ functional-programming-technique/src/test/java/info/pragmaticdeveloper/java8/CollectorUnitTest.java
@@ -1,14 +1,20 @@
 package info.pragmaticdeveloper.java8;
 
 import info.pragmaticdeveloper.domain.Dish;
+import info.pragmaticdeveloper.domain.Dish.Type;
 import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
+import static info.pragmaticdeveloper.domain.Dish.Type.*;
 import static info.pragmaticdeveloper.util.TestDataProvider.buildMenu;
 import static java.util.Comparator.comparing;
+import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
@@ -19,10 +25,35 @@ public class CollectorUnitTest {
     void testMaxCalorieDish() {
         var result = buildMenu().stream()
                 .max(comparing(Dish::getCalories)).get();
-        assertThat(result, equalTo(new Dish("pork", false, 800, Dish.Type.MEAT)));
+        assertThat(result, equalTo(new Dish("pork", false, 800, MEAT)));
         result = buildMenu().stream()
                 .collect(maxBy(comparing(Dish::getCalories))).get();
-        assertThat(result, equalTo(new Dish("pork", false, 800, Dish.Type.MEAT)));
+        assertThat(result, equalTo(new Dish("pork", false, 800, MEAT)));
+    }
+
+    @Test
+    void testDishTypeByDecreasingOrderOfCalories() {
+        Map<Dish.Type, Integer> expected = new LinkedHashMap<>();
+        expected.put(MEAT, 1900);
+        expected.put(OTHER, 1550);
+        expected.put(FISH, 750);
+        var result = buildMenu().stream()
+                .collect(groupingBy(Dish::getType))
+                .entrySet()
+                .stream()
+                .collect(toMap(Map.Entry::getKey, e -> sumCalories(e.getValue())))
+                .entrySet()
+                .stream()
+                .sorted(comparingInt(Map.Entry<Dish.Type, Integer>::getValue).reversed().thenComparing(Map.Entry::getKey))
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (v1, v2) -> v1, LinkedHashMap::new));
+        assertThat(result, equalTo(expected));
+    }
+
+    private int sumCalories(List<Dish> dishes) {
+        return dishes.stream()
+                .map(Dish::getCalories)
+                .mapToInt(calories -> calories)
+                .sum();
     }
 
     @Test
