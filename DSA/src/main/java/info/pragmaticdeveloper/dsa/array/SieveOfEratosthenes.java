1803ce02d3cc0cf6c1a69160a19ca32f8871ec07
astha-iOS <asthayadavit@gmail.com>
2023-02-25 22:23:39 +0100

    prime using sieve

--- DSA/src/main/java/info/pragmaticdeveloper/dsa/array/SieveOfEratosthenes.java
+++ DSA/src/main/java/info/pragmaticdeveloper/dsa/array/SieveOfEratosthenes.java
@@ -1,5 +1,7 @@
 package info.pragmaticdeveloper.dsa.array;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -10,13 +12,44 @@ public class SieveOfEratosthenes {
                 .boxed()
                 .collect(Collectors.toSet());
         for (int i = 2; i <= ((int) Math.sqrt(end)) + 1; i++) {
-            for (int j = i; i * j <= end + 1; j++) {
-                prime.remove(i * j);
+            // if number has already been remove then do not need check it.
+            if (prime.contains(i)) {
+                for (int j = i; i * j <= end + 1; j++) {
+                    prime.remove(i * j);
+                }
             }
         }
         return prime
                 .stream()
                 .mapToInt(i -> i)
                 .toArray();
+    }
+
+    //https://www.khanacademy.org/computing/computer-science/cryptography/comp-number-theory/v/sieve-of-eratosthenes-prime-adventure-part-4
+    public int[] getPrimeUpToNumber(int inclusiveEnd) {
+        if (inclusiveEnd < 2) {
+            return new int[]{};
+        }
+        boolean[] isPrime = new boolean[inclusiveEnd + 1];
+        for (int i = 2; i <= inclusiveEnd; i++) {
+            isPrime[i] = true;
+        }
+        List<Integer> primeNumber = new ArrayList<>();
+        for (int i = 2; i <= (int) Math.sqrt(inclusiveEnd) + 1; i++) {
+            if (isPrime[i]) {
+                primeNumber.add(i);
+                // start with square of number and mark all its multiple as non prime. i.e composite
+                for (int j = i * i; j <= inclusiveEnd; j = j + i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = (int) Math.sqrt(inclusiveEnd) + 2; i < isPrime.length; i++) {
+            if (isPrime[i]) {
+                primeNumber.add(i);
+            }
+        }
+        return primeNumber.stream().mapToInt(i -> i).toArray();
     }
 }
--- DSA/src/test/java/info/pragmaticdeveloper/dsa/array/SieveOfEratosthenesTest.java
+++ DSA/src/test/java/info/pragmaticdeveloper/dsa/array/SieveOfEratosthenesTest.java
@@ -18,11 +18,19 @@ class SieveOfEratosthenesTest {
         assertThat(result, is(expected));
     }
 
+    @ParameterizedTest
+    @MethodSource("testDataProvider")
+    void getPrimeUpToUsingSieve(int end, int[] expected) {
+        var result = new SieveOfEratosthenes().getPrimeUpToNumber(end);
+        assertThat(result, is(expected));
+    }
+
     private static Stream<Arguments> testDataProvider() {
         return Stream.of(
                 Arguments.of(2, new int[]{2}),
                 Arguments.of(5, new int[]{2, 3, 5}),
-                Arguments.of(20, new int[]{2, 3, 5, 7, 11, 13, 17, 19})
+                Arguments.of(20, new int[]{2, 3, 5, 7, 11, 13, 17, 19}),
+                Arguments.of(30, new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29})
         );
     }
 }
